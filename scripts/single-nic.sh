c6b08b1424bf8f1672a67f896b40fcda4ae73208
Clay Risser <clayrisser@gmail.com>
2024-05-18 16:37:13 -0400

    Initial commit

---
+++ scripts/single-nic.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+OPENSTACK_VERSION=2023.2
+
+sudo true
+if ! which yq >/dev/null 2>&1; then
+    sudo snap install yq
+fi
+NETCFG_PATH="/etc/netplan/$(ls /etc/netplan 2>/dev/null)"
+if [ "$NETCFG_PATH" = "/etc/netplan/" ]; then
+    echo "netplan required" 1>&2
+    exit 1
+fi
+NIC="$(sudo cat "$NETCFG_PATH" | yq -r '(.network.ethernets | keys)[0]' 2>/dev/null)"
+if [ "$NIC" = "" ]; then
+    echo "no nic found" 1>&2
+    exit 1
+fi
+
+cat <<EOF > netcfg-0.yaml
+network:
+  vlans:
+    eno1:
+      id: 10
+      link: $NIC
+      addresses: [172.16.1.201/24]
+      nameservers:
+        addresses: [185.12.64.1, 185.12.64.2]
+      routes:
+        - to: 0.0.0.0/0
+          via: 172.16.1.1
+          table: 100
+      routing-policy:
+        - from: 172.16.1.0/24
+          table: 100
+    eno2:
+      id: 20
+      link: $NIC
+      addresses: [172.16.2.2/24]
+      nameservers:
+        addresses: [185.12.64.1, 185.12.64.2]
+      routes:
+        - to: 0.0.0.0/0
+          via: 172.16.2.1
+          table: 200
+      routing-policy:
+        - from: 172.16.2.0/24
+          table: 200
+EOF
+sudo cat "$NETCFG_PATH" | yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' - netcfg-0.yaml > netcfg-1.yaml
+cat netcfg-1.yaml | sudo tee "$NETCFG_PATH"
+rm netcfg-0.yaml netcfg-1.yaml
+sudo netplan apply
+sudo snap install openstack --channel "$OPENSTACK_VERSION"
+sunbeam prepare-node-script
+bash -x && newgrp snap_daemon
+printf "To observe, run each command in a new session:\n\n"
+printf "    \e[32mwatch --color -- juju status --color -m openstack\e[0m\n"
+printf "    \e[32mwatch sudo minikube.kubectl get all -A\e[0m\n\n"
+sunbeam cluster bootstrap --accept-defaults
