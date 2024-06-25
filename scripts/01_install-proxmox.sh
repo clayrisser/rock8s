fdfa60ec725b1355b3ef97e5ea0bbd3950be0bd1
Clay Risser <clayrisser@gmail.com>
2024-06-25 03:53:16 -0500

    Created initial proxmox setup

---
+++ scripts/01_install-proxmox.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PROXMOX_VERSION=8.2-1
+PROXMOX_ISO="https://enterprise.proxmox.com/iso/proxmox-ve_${PROXMOX_VERSION}.iso"
+DRIVE_INTERFACE="nvme"
+
+SUDO=
+if which sudo >/dev/null 2>&1; then
+    SUDO=sudo
+fi
+$SUDO true
+if ! which curl >/dev/null 2>&1; then
+    $SUDO apt-get update && $SUDO apt-get install -y curl
+fi
+if [ ! -f pve.iso ]; then
+    curl -Lo pve.iso "$PROXMOX_ISO"
+fi
+DRIVES="$(lsblk -dn -o NAME | grep "$DRIVE_INTERFACE")"
+DRIVE_ARGS=""
+for DRIVE in $DRIVES; do
+    DRIVE_ARGS="$DRIVE_ARGS -drive file=/dev/$DRIVE,format=raw,media=disk,if=virtio"
+done
+qemu-system-x86_64 -enable-kvm -bios /usr/share/ovmf/OVMF.fd \
+    -k en-us -cpu host -smp 4 -m 4096 -boot d -cdrom ./pve.iso \
+    $DRIVE_ARGS \
+    -vnc :0
