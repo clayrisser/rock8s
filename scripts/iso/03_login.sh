0de1cdbcb7471e1333920b4b6c9cf4eff33224a7
Clay Risser <clayrisser@gmail.com>
2024-06-29 16:20:18 -0400

    Updated interface

--- scripts/iso/03_login.sh
+++ scripts/iso/03_login.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-INTERFACE="$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/ .*//")"
-INTERFACE_ALTNAME="$(ip addr show $INTERFACE | grep -E "^ *altname" | awk '{ print $2 }' | head -n1)"
-IP_ADDRESS_CIDR="$(ip addr show $INTERFACE | grep -E "^ *inet" | awk '{ print $2 }' | head -n1)"
 GATEWAY="$(ip route | grep default | awk '{ print $3 }')"
+INTERFACE="$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/ .*//")"
+PUBLIC_IP_ADDRESS_CIDR="$(ip addr show $INTERFACE | grep -E "^ *inet" | awk '{ print $2 }' | head -n1)"
+if [ "$INTERFACE" = "vmbr0" ]; then
+    INTERFACE="$(ip addr | grep "vmbr0 state UP" | sed 's|^[0-9]*:\s*||g' | cut -d':' -f1)"
+else
+    _INTERFACE="$(ip addr show $INTERFACE | grep -E "^ *altname" | awk '{ print $2 }' | head -n1)"
+    if [ "$_INTERFACE" != "" ]; then
+        INTERFACE="$_INTERFACE"
+    fi
+fi
 cat <<EOF > .env
 GATEWAY=$GATEWAY
-IP_ADDRESS_CIDR=$IP_ADDRESS_CIDR
+PUBLIC_IP_ADDRESS_CIDR=$PUBLIC_IP_ADDRESS_CIDR
 INTERFACE=$INTERFACE
-INTERFACE_ALTNAME=$INTERFACE_ALTNAME
 EOF
 scp -P 2222 .env root@localhost:/root/
 ssh -p 2222 root@localhost
