9ee70603dd57359f21ebc9ca8479ee058f6874b4
Clay Risser <clayrisser@gmail.com>
2024-07-02 09:27:38 -0400

    Created initial kubernetes config

---
+++ kubernetes/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">=1.3.3"
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.14"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure     = var.pm_tls_insecure
+  pm_parallel         = var.pm_parallel
+  pm_timeout          = var.pm_timeout
+}
