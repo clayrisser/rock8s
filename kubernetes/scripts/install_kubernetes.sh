e0f3eff1ddd40080db6f084fe99bb7462a17cede
Clay Risser <clayrisser@gmail.com>
2024-07-04 10:50:27 -0400

    Updated scripts

--- kubernetes/scripts/install_kubernetes.sh
+++ kubernetes/scripts/install_kubernetes.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-kubespray_docker_image="${kubespray_docker_image}"
-kubespray_data_dir="${kubespray_data_dir}"
+#!/bin/sh
 
-# Install Kubernetes
-sudo docker run \
-    --mount type=bind,source="$kubespray_data_dir/inventory.ini",dst=/inventory/sample/inventory.ini \
-    --mount type=bind,source="$kubespray_data_dir/addons.yml",dst=/inventory/sample/group_vars/k8s_cluster/addons.yml \
-    --mount type=bind,source="$kubespray_data_dir/k8s-cluster.yml",dst=/inventory/sample/group_vars/k8s_cluster/k8s-cluster.yml \
-    --mount type=bind,source="$kubespray_data_dir/id_rsa",dst=/root/.ssh/id_rsa \
-    $kubespray_docker_image bash -c \
+KUBESPRAY_DOCKER_IMAGE="${kubespray_docker_image}"
+KUBESPRAY_DATA_DIR="${kubespray_data_dir}"
+sudo docker run --rm \
+    --mount type=bind,source="$KUBESPRAY_DATA_DIR/inventory.ini",dst=/inventory/sample/inventory.ini \
+    --mount type=bind,source="$KUBESPRAY_DATA_DIR/addons.yml",dst=/inventory/sample/group_vars/k8s_cluster/addons.yml \
+    --mount type=bind,source="$KUBESPRAY_DATA_DIR/k8s-cluster.yml",dst=/inventory/sample/group_vars/k8s_cluster/k8s-cluster.yml \
+    --mount type=bind,source="$KUBESPRAY_DATA_DIR/id_rsa",dst=/root/.ssh/id_rsa \
+    "$KUBESPRAY_DOCKER_IMAGE" sh -c \
     "ansible-playbook -i /inventory/sample/inventory.ini -u admin -become cluster.yml"
