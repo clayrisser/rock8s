97f897e774db2a8c3015589a3776a1044b2499ce
Clay Risser <clayrisser@gmail.com>
2024-07-08 15:32:24 -0400

    Updated scripts

--- scripts/list-interfaces-by-role.sh
+++ scripts/list-interfaces-by-role.sh
@@ -24,6 +24,6 @@ else
     _UPLINK=$1
     _CEPH=$2
 fi
-echo "$_UPLINK=uplink:$($interface_data | grep -E "^$_UPLINK=" | sed "s|^$_UPLINK=||g")"
-echo "$_CEPH=ceph:$($interface_data | grep -E "^$_CEPH=" | sed "s|^$_CEPH=||g")"
-echo "$_PRIVATE=private:$($interface_data | grep -E "^$_PRIVATE=" | sed "s|^$_PRIVATE=||g")"
+echo "$_UPLINK=uplink:$($_INTERFACE_DATA | grep -E "^$_UPLINK=" | sed "s|^$_UPLINK=||g")"
+echo "$_CEPH=ceph:$($_INTERFACE_DATA | grep -E "^$_CEPH=" | sed "s|^$_CEPH=||g")"
+echo "$_PRIVATE=private:$($_INTERFACE_DATA | grep -E "^$_PRIVATE=" | sed "s|^$_PRIVATE=||g")"
