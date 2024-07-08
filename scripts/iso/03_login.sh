9ab62b11f6fb6def30c9b5d000ed53b35fbfdf2b
Clay Risser <clayrisser@gmail.com>
2024-07-08 17:45:35 -0400

    Updated scripts

--- scripts/iso/03_login.sh
+++ scripts/iso/03_login.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
 GATEWAY="$(ip route | grep default | awk '{ print $3 }')"
-INTERFACE="$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/ .*//")"
-PUBLIC_IP_ADDRESS_CIDR="$(ip addr show $INTERFACE | grep -E "^ *inet" | awk '{ print $2 }' | head -n1)"
-if [ "$INTERFACE" = "vmbr0" ]; then
-    INTERFACE="$(ip addr | grep "vmbr0 state UP" | sed 's|^[0-9]*:\s*||g' | cut -d':' -f1)"
-else
-    _INTERFACE="$(ip addr show $INTERFACE | grep -E "^ *altname" | awk '{ print $2 }' | head -n1)"
-    if [ "$_INTERFACE" != "" ]; then
-        INTERFACE="$_INTERFACE"
-    fi
-fi
-cat <<EOF > .env
+PUBLIC_IP_ADDRESS_CIDR="$(ip addr show "$(ip route | awk '/default via/ {print $5}')" | \
+    grep -E "^ *inet" | awk '{ print $2 }' | head -n1)"
+NETWORK_DEVICES_BY_ROLE="$(curl -Lsf https://gitlab.com/bitspur/rock8s/yaps/-/raw/main/scripts/list-network-devices-by-role.sh | sh)"
+cat <<EOF | tee .env
 GATEWAY=$GATEWAY
 PUBLIC_IP_ADDRESS_CIDR=$PUBLIC_IP_ADDRESS_CIDR
-INTERFACE=$INTERFACE
+NETWORK_DEVICES_BY_ROLE="$NETWORK_DEVICES_BY_ROLE"
 EOF
 scp -P 2222 .env root@localhost:/root/
 ssh -p 2222 root@localhost
