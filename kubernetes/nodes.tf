4bfa153b31b0760344df34a951d964dddaa8157e
Clay Risser <clayrisser@gmail.com>
2024-07-08 08:16:58 -0400

    Updated nodes

--- kubernetes/nodes.tf
+++ kubernetes/nodes.tf
@@ -1,16 +1,16 @@
 module "k8s_control_plane_nodes" {
   source              = "./modules/proxmox_vm"
-  node_count          = var.vm_k8s_control_plane["node_count"]
+  node_count          = var.vm_k8s_control_plane_node_count
   proxmox_node        = var.proxmox_node
   vm_clone            = var.vm_clone
   vm_name_prefix      = "vm-${local.cluster_name}-cp"
   vm_max_vcpus        = var.vm_max_vcpus
-  vm_vcpus            = var.vm_k8s_control_plane["vcpus"]
+  vm_vcpus            = var.vm_k8s_control_plane_vcpus
   vm_sockets          = var.vm_sockets
   vm_cpu_type         = var.vm_cpu_type
-  vm_memory_mb        = var.vm_k8s_control_plane["memory"]
+  vm_memory_mb        = var.vm_k8s_control_plane_memory
   vm_os_disk_storage  = var.vm_os_disk_storage
-  vm_os_disk_size_gb  = var.vm_k8s_control_plane["disk_size"]
+  vm_os_disk_size_gb  = var.vm_k8s_control_plane_disk_size
   vm_net_name         = var.internal_net_name
   vm_net_subnet_cidr  = var.internal_net_subnet_cidr
   vm_user             = var.vm_user
@@ -20,17 +20,17 @@ module "k8s_control_plane_nodes" {
 
 module "k8s_worker_nodes" {
   source                        = "./modules/proxmox_vm"
-  node_count                    = var.vm_k8s_worker["node_count"]
+  node_count                    = var.vm_k8s_worker_node_count
   proxmox_node                  = var.proxmox_node
   vm_clone                      = var.vm_clone
   vm_name_prefix                = "vm-${local.cluster_name}-worker"
   vm_max_vcpus                  = var.vm_max_vcpus
-  vm_vcpus                      = var.vm_k8s_worker["vcpus"]
+  vm_vcpus                      = var.vm_k8s_worker_vcpus
   vm_sockets                    = var.vm_sockets
   vm_cpu_type                   = var.vm_cpu_type
-  vm_memory_mb                  = var.vm_k8s_worker["memory"]
+  vm_memory_mb                  = var.vm_k8s_worker_memory
   vm_os_disk_storage            = var.vm_os_disk_storage
-  vm_os_disk_size_gb            = var.vm_k8s_worker["disk_size"]
+  vm_os_disk_size_gb            = var.vm_k8s_worker_disk_size
   vm_net_name                   = var.internal_net_name
   vm_net_subnet_cidr            = var.internal_net_subnet_cidr
   vm_user                       = var.vm_user
