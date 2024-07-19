a2d2f5026b36916ba981f4980456d5f825588e01
Clay Risser <clayrisser@gmail.com>
2024-07-19 12:40:09 -0400

    Updated packer networking

--- images/debian-12/variables.mk
+++ images/debian-12/variables.mk
@@ -2,14 +2,13 @@ VARS := -var cores=$(CORES) \
 		-var cpu_type=$(CPU_TYPE) \
 		-var disk_format=$(DISK_FORMAT) \
 		-var disk_size=$(DISK_SIZE) \
-		-var gateway=$(GATEWAY) \
-		-var ip=$(IP) \
 		-var iso_checksum=$(ISO_CHECKSUM) \
 		-var iso_file=$(ISO_FILE) \
 		-var iso_storage_pool=$(ISO_STORAGE_POOL) \
 		-var iso_url=$(ISO_URL) \
 		-var memory=$(MEMORY) \
 		-var network_bridge=$(NETWORK_BRIDGE) \
+		-var network_ip=$(NETWORK_IP) \
 		-var proxmox_host=$(PROXMOX_HOST) \
 		-var proxmox_node=$(PROXMOX_NODE) \
 		-var proxmox_token_id=$(PROXMOX_TOKEN_ID) \
