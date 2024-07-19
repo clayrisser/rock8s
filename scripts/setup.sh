f6ecaaf5697a9a6001d639c74eca08cf867190ed
Clay Risser <clayrisser@gmail.com>
2024-07-19 09:10:19 -0400

    Sync yaps

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+YAPS_REPO="https://gitlab.com/bitspur/rock8s/yaps.git"
 IMAGES="
 https://cdimage.debian.org/mirror/cdimage/archive/12.5.0/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso
 https://download.fedoraproject.org/pub/fedora/linux/releases/40/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-40-1.14.iso
 "
+
+if [ ! "$USER" = "admin" ]; then
+    echo "this script must be run as admin user" 1>&2
+    exit 1
+fi
+if ! sudo pvesh get /cluster/status --output-format json | jq -e '.[0].quorate' >/dev/null; then
+    echo "this script must be run on a Proxmox cluster" 1>&2
+    exit 1
+fi
+if ! [ -d "/mnt/pve/cephfs" ]; then
+    echo "cephfs filesystem is required" 1>&2
+    exit 1
+fi
 sudo true
 export DEBIAN_FRONTEND=noninteractive
 wget -qO- https://download.ceph.com/keys/release.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ceph.gpg >/dev/null
@@ -27,10 +41,11 @@ mkdir -p "$HOME/.ssh"
 if [ ! -f "$HOME/.ssh/id_rsa" ]; then
     ssh-keygen -t rsa -b 4096 -C "$(whoami)@$_DOMAIN" -N "" -f "$HOME/.ssh/id_rsa"
 fi
-sudo rm -rf /etc/pve/tmp 2>/dev/null || true
-sudo mkdir -p /etc/pve/tmp
-sudo cp "$HOME/.ssh/id_rsa" /etc/pve/tmp
-sudo cp "$HOME/.ssh/id_rsa.pub" /etc/pve/tmp
+sudo rm -rf /mnt/pve/cephfs/shared/tmp 2>/dev/null || true
+sudo mkdir -p /mnt/pve/cephfs/shared/tmp
+sudo chown -R $USER:$USER /mnt/pve/cephfs/shared
+sudo cp "$HOME/.ssh/id_rsa" /mnt/pve/cephfs/shared/tmp
+sudo cp "$HOME/.ssh/id_rsa.pub" /mnt/pve/cephfs/shared/tmp
 if [ ! -f "$HOME/.ssh/authorized_keys" ]; then
     touch "$HOME/.ssh/authorized_keys"
     chmod 600 "$HOME/.ssh/authorized_keys"
@@ -38,13 +53,22 @@ fi
 if ! grep -qxF "$(cat "$HOME/.ssh/id_rsa.pub")" "$HOME/.ssh/authorized_keys"; then
     cat "$HOME/.ssh/id_rsa.pub" >> "$HOME/.ssh/authorized_keys"
 fi
+if [ ! -d $HOME/shared ]; then
+    ln -s /mnt/pve/cephfs/shared $HOME/shared
+fi
+if [ ! -L "$HOME/yaps" ]; then
+    sudo rm -rf "$HOME/yaps"
+    git clone "$YAPS_REPO" /mnt/pve/cephfs/shared/yaps
+    ln -s /mnt/pve/cephfs/shared/yaps "$HOME/yaps"
+fi
+(cd "$HOME/yaps" && git pull origin main)
 _NODES=$(sudo pvesh get /nodes --output-format json | jq -r '.[].node' | sort)
 for _NODE in $_NODES; do
     _NODE_ID=$(sudo corosync-cmapctl | grep -oP "(?<=nodelist.node.)\d+(?=.name \(str\) = $_NODE)")
     _NODE_IP=$(sudo corosync-cmapctl | grep "nodelist.node.$_NODE_ID.ring0_addr" | awk -F' = ' '{print $2}')
     if [ "$_NODE_IP" != "" ]; then
         if ! grep -q "$_NODE_IP" /etc/hosts; then
-            ssh admin@$_NODE_IP "
+            ssh $USER@$_NODE_IP "
                 wget -qO- https://download.ceph.com/keys/release.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ceph.gpg >/dev/null
                 sudo apt-add-repository -y \"deb https://download.ceph.com/debian-reef/ \$(lsb_release -cs) main\"
                 sudo systemctl mask rpcbind
@@ -55,23 +79,30 @@ for _NODE in $_NODES; do
                 sudo apt-get install -y \
                     packer \
                     terraform
-                sudo mkdir -p /home/admin/.ssh
-                sudo cp /etc/pve/tmp/id_rsa /home/admin/.ssh/id_rsa
-                sudo cp /etc/pve/tmp/id_rsa.pub /home/admin/.ssh/id_rsa.pub
-                sudo chown admin:admin /home/admin/.ssh/id_rsa
-                sudo chown admin:admin /home/admin/.ssh/id_rsa.pub
-                sudo chmod 600 /home/admin/.ssh/id_rsa
-                sudo chmod 644 /home/admin/.ssh/id_rsa.pub
-                if [ ! -f /home/admin/.ssh/authorized_keys ]; then
-                    sudo touch /home/admin/.ssh/authorized_keys
-                    sudo chmod 600 /home/admin/.ssh/authorized_keys
+                sudo mkdir -p /home/$USER/.ssh
+                sudo cp /mnt/pve/cephfs/shared/tmp/id_rsa /home/$USER/.ssh/id_rsa
+                sudo cp /mnt/pve/cephfs/shared/tmp/id_rsa.pub /home/$USER/.ssh/id_rsa.pub
+                sudo chown $USER:$USER /home/$USER/.ssh/id_rsa
+                sudo chown $USER:$USER /home/$USER/.ssh/id_rsa.pub
+                sudo chmod 600 /home/$USER/.ssh/id_rsa
+                sudo chmod 644 /home/$USER/.ssh/id_rsa.pub
+                if [ ! -f /home/$USER/.ssh/authorized_keys ]; then
+                    touch /home/$USER/.ssh/authorized_keys
+                    chmod 600 /home/$USER/.ssh/authorized_keys
                 fi
-                if ! grep -qxF \"\$(cat /home/admin/.ssh/id_rsa.pub)\" /home/admin/.ssh/authorized_keys; then
-                    sudo cat /home/admin/.ssh/id_rsa.pub >> /home/admin/.ssh/authorized_keys
+                if ! grep -qxF \"\$(cat /home/$USER/.ssh/id_rsa.pub)\" /home/$USER/.ssh/authorized_keys; then
+                    sudo cat /home/$USER/.ssh/id_rsa.pub >> /home/$USER/.ssh/authorized_keys
+                fi
+                if [ ! -d /home/$USER/shared ]; then
+                    ln -s /mnt/pve/cephfs/shared /home/$USER/shared
+                fi
+                if [ ! -L "/home/$USER/yaps" ]; then
+                    sudo rm -rf "/home/$USER/yaps"
+                    ln -s /mnt/pve/cephfs/shared/yaps /home/$USER/yaps
                 fi
             "
         fi
     fi
 done
-sudo rm -rf /etc/pve/tmp
-make -sC $HOME/yaps images/build
+sudo rm -rf /mnt/pve/cephfs/shared/tmp
+make -sC "$HOME/yaps" images/build
