8c9d064613a9879171f348e9eca5c9ee802e980d
Clay Risser <clayrisser@gmail.com>
2024-08-10 02:48:58 +0000

    Added power dns

--- powerdns/scripts/install.sh
+++ powerdns/scripts/install.sh
@@ -3,23 +3,39 @@
 export DEBIAN_FRONTEND=noninteractive
 alias tmpl="sh ../scripts/tmpl.sh"
 HOST_NUMBER="$(echo "$(hostname)" | sed 's/[^0-9]//g')"
-export SOA_CONTENT="ns$HOST_NUMBER.@ hostmaster.@ 0 10800 3600 604800 3600"
-
+_SOA_NS_HOSTNAME="$(sudo cat /mnt/pve/cephfs/shared/nameservers.yaml | perl -MYAML::XS=Load -MJSON=encode_json -E 'say encode_json(Load(do { local $/; <STDIN> }))' | jq -r ".$(hostname).ns")"
+if [ "$_SOA_NS_HOSTNAME" = "" ]; then
+	_SOA_NS_HOSTNAME="ns$HOST_NUMBER.@"
+fi
+export SOA_CONTENT="$_SOA_NS_HOSTNAME hostmaster.@ 0 10800 3600 604800 3600"
 for n in $(sudo pvesh get /nodes --output-format json | jq -r '.[].node' | sort); do
-	_IP="$(sudo grep -A 5 "name: $n" /etc/pve/corosync.conf | grep 'ring0_addr' | cut -d ':' -f 2 | tr -d ' ')"
 	_HOST_NUMBER="$(echo "$n" | sed 's/[^0-9]//g')"
-	if ! grep -q "ns$_HOST_NUMBER.$(hostname -d)" /etc/hosts; then
-		echo "$_IP ns$_HOST_NUMBER.$(hostname -d)" | sudo tee -a /etc/hosts >/dev/null
+	if [ -f /mnt/pve/cephfs/shared/nameservers.yaml ]; then
+		_OUT="$(sudo cat /mnt/pve/cephfs/shared/nameservers.yaml | perl -MYAML::XS=Load -MJSON=encode_json -E 'say encode_json(Load(do { local $/; <STDIN> }))')"
+		_NS_HOSTNAME="$(echo "$_OUT" | jq -r ".$n.ns // empty")"
+		_NS_IP="$(echo "$_OUT" | jq -r ".$n.ip // empty")"
+	else
+		_NS_HOSTNAME="ns$_HOST_NUMBER.$(hostname -d)"
+	fi
+	if [ "$_NS_IP" = "" ]; then
+		_NS_IP="$(sudo grep -A 5 "name: $n" /etc/pve/corosync.conf | grep 'ring0_addr' | cut -d ':' -f 2 | tr -d ' ')"
+	fi
+	if [ "$_NS_HOSTNAME" != "" ] && [ "$_NS_IP" != "" ] && ! grep -q "$_NS_HOSTNAME" /etc/hosts; then
+		echo "$_NS_IP $_NS_HOSTNAME" | sudo tee -a /etc/hosts >/dev/null
 	fi
 done
 if [ "$HOST_NUMBER" = "1" ]; then
 	SECONDARY_IPS=""
 	for n in $(sudo pvesh get /nodes --output-format json | jq -r '.[].node' | sort | grep -v $(hostname) | head -n1); do
-		_SECONDARY_IPS="$(sudo grep -A 5 "name: $n" /etc/pve/corosync.conf | grep 'ring0_addr' | cut -d ':' -f 2 | tr -d ' ')"
+		_OUT="$(sudo cat /mnt/pve/cephfs/shared/nameservers.yaml | perl -MYAML::XS=Load -MJSON=encode_json -E 'say encode_json(Load(do { local $/; <STDIN> }))')"
+		_NS_IP="$(echo "$_OUT" | jq -r ".$n.ip // empty")"
+		if [ "$_NS_IP" = "" ]; then
+			_NS_IP="$(sudo grep -A 5 "name: $n" /etc/pve/corosync.conf | grep 'ring0_addr' | cut -d ':' -f 2 | tr -d ' ')"
+		fi
 		if [ "$SECONDARY_IPS" = "" ]; then
-			SECONDARY_IPS="$_SECONDARY_IPS"
+			SECONDARY_IPS="$_NS_IP"
 		else
-			SECONDARY_IPS="$SECONDARY_IPS,$_SECONDARY_IPS"
+			SECONDARY_IPS="$SECONDARY_IPS,$_NS_IP"
 		fi
 	done
     export PRIMARY_SECONDARY="master=yes
