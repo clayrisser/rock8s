c0590289bc50f2860d909bdcbd5720be28d7f9f2
Clay Risser <clayrisser@gmail.com>
2024-09-05 20:37:47 +0000

    Removed kubeflow and added avp argocd plugin

--- kubernetes/cluster/modules/argocd/module.mk
+++ kubernetes/cluster/modules/argocd/module.mk
@@ -17,6 +17,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq (,$(wildcard $(CURDIR)/artifacts/kubeconfig.json))
-export ARGOCD_AUTH_PASSWORD ?= $(shell kubectl --kubeconfig $(KUBECONFIG_JSON) -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' 2>/dev/null | base64 --decode)
-endif
+export ARGOCD_AUTH_PASSWORD ?= $(shell kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' 2>/dev/null | base64 --decode)
