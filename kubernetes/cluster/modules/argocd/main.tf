1e42f78f47cefa142013d362680a83e869e006a6
Clay Risser <clayrisser@gmail.com>
2024-09-16 22:44:59 +0000

    Removed argo plugins

--- kubernetes/cluster/modules/argocd/main.tf
+++ kubernetes/cluster/modules/argocd/main.tf
@@ -43,68 +43,6 @@ global:
 configs:
   params:
     server.disable.auth: true
-  cmp:
-    create: true
-    plugins:
-      avp:
-        discover:
-          find:
-            command: 
-              - sh
-              - "-c"
-              - "find . -name '*.yaml'"
-        generate:
-          command:
-            - argocd-vault-plugin"
-            - generate
-            - "."
-repoServer:
-  volumes:
-    - name: custom-tools
-      emptyDir: {}
-    - name: cmp-plugin
-      configMap:
-        name: argocd-cmp-cm
-  volumeMounts:
-    - name: custom-tools
-      mountPath: /usr/local/bin/argocd-vault-plugin
-      subPath: argocd-vault-plugin
-  extraContainers:
-    - name: avp
-      command: [/var/run/argocd/argocd-cmp-server]
-      image: quay.io/argoproj/argocd:v2.7.2
-      securityContext:
-        runAsNonRoot: true
-        runAsUser: 999
-      volumeMounts:
-        - mountPath: /var/run/argocd
-          name: var-files
-        - mountPath: /home/argocd/cmp-server/plugins
-          name: plugins
-        - mountPath: /tmp
-          name: tmp
-        - mountPath: /home/argocd/cmp-server/config/plugin.yaml
-          subPath: avp.yaml
-          name: cmp-plugin
-        - name: custom-tools
-          subPath: argocd-vault-plugin
-          mountPath: /usr/local/bin/argocd-vault-plugin
-  initContainers:
-    - name: download-tools
-      image: alpine:3.8
-      command: [sh, -c]
-      env:
-        - name: AVP_VERSION
-          value: "1.14.0"
-      args:
-        - >-
-          wget -O argocd-vault-plugin
-          https://github.com/argoproj-labs/argocd-vault-plugin/releases/download/v$${AVP_VERSION}/argocd-vault-plugin_$${AVP_VERSION}_linux_amd64 &&
-          chmod +x argocd-vault-plugin &&
-          mv argocd-vault-plugin /custom-tools/
-      volumeMounts:
-        - mountPath: /custom-tools
-          name: custom-tools
 EOF
     ,
     var.values
