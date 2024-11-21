4b2a4d62c7b40ac01930bb943fb5261f147f11ef
Lalit Rajak <lalitrajak312@gmail.com>
2024-11-21 21:13:10 +0000

    Added longhorn support

--- kubernetes/cluster/external_dns.tf
+++ kubernetes/cluster/external_dns.tf
@@ -1,15 +1,14 @@
 module "external-dns" {
   source  = "./modules/external_dns"
   enabled = var.external_dns
-  targets = var.cluster_entrypoint
   dns_providers = {
-    # pdns = {
-    #   api_url = var.pdns_api_url
-    #   api_key = var.pdns_api_key
-    # }
-    # hetzner = {
-    #   api_key = var.hetzner_api_key
-    # }
+    pdns = {
+      api_url = var.pdns_api_url
+      api_key = var.pdns_api_key
+    }
+    hetzner = {
+      api_key = var.hetzner_api_key
+    }
     cloudflare = {
       api_key = var.cloudflare_api_key
       email   = var.cloudflare_email
