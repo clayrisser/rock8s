367169eaf443b1a9cd133650708ebf1df0915810
Clay Risser <clayrisser@gmail.com>
2024-12-10 04:24:20 +0000

    Update 02_boot.sh

--- scripts/iso/02_boot.sh
+++ scripts/iso/02_boot.sh
@@ -7,6 +7,7 @@ for DRIVE in $DRIVES; do
     DRIVE_ARGS="$DRIVE_ARGS -drive file=/dev/$DRIVE,format=raw,media=disk,if=virtio"
 done
 qemu-system-x86_64 -enable-kvm -bios /usr/share/ovmf/OVMF.fd -cpu host \
+    -enable-kvm -machine type=q35,accel=kvm \
     -k en-us -device virtio-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:2222-:22 -smp 4 -m 4096 \
     $DRIVE_ARGS \
     -vnc :0
