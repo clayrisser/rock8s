e102bc42db86711f4f372327be3b9653e3330a7b
Clay Risser <clayrisser@gmail.com>
2025-01-08 00:26:33 -0600

    Fixed qemu install

--- scripts/iso/01_boot-installer.sh
+++ scripts/iso/01_boot-installer.sh
@@ -20,11 +20,19 @@ fi
 DRIVES="$(lsblk -dn -o NAME | grep -E "^$DRIVE_PREFIX" | sort | head -n2)"
 DRIVE_ARGS=""
 for DRIVE in $DRIVES; do
-    DRIVE_ARGS="$DRIVE_ARGS -drive file=/dev/$DRIVE,format=raw,if=virtio,cache=none,aio=native"
+    DRIVE_ARGS="$DRIVE_ARGS -drive file=/dev/$DRIVE,format=raw,if=virtio"
 done
 qemu-system-x86_64 \
     -enable-kvm \
     -machine type=q35,accel=kvm \
+    -bios /usr/share/ovmf/OVMF.fd \
     -k en-us -cpu host -smp 4 -m 4096 -boot d -cdrom ./pve.iso \
+    -global driver=cfi.pflash01,property=secure,value=on \
+    -global ICH9-LPC.disable_s3=1 \
+    -global ICH9-LPC.disable_s4=1 \
+    -boot menu=on,strict=on \
+    -device virtio-vga \
+    -device intel-hda \
+    -device virtio-net-pci \
     $DRIVE_ARGS \
     -vnc :0
