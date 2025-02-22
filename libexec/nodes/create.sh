8416aeb3d27aaa5d93676f09e8bb71ad401946ba
Clay Risser <clayrisser@gmail.com>
2025-02-22 03:37:23 -0600

    Added nodes destroy

--- libexec/nodes/create.sh
+++ libexec/nodes/create.sh
@@ -212,7 +212,7 @@ users:
     cd "$CLUSTER_DIR/provider"
     terraform init -backend=true -backend-config="path=$_PURPOSE_DIR/terraform.tfstate" >&2
     terraform apply -auto-approve -state="$_PURPOSE_DIR/terraform.tfstate" -var-file="$_PURPOSE_DIR/terraform.tfvars.json" >&2
-    terraform output -json > "$_PURPOSE_DIR/output.json" >&2
+    terraform output -state="$_PURPOSE_DIR/terraform.tfstate" -json > "$_PURPOSE_DIR/output.json"
     printf '{"cluster":"%s","provider":"%s","tenant":"%s","purpose":"%s"}\n' \
         "$_CLUSTER" "$_PROVIDER" "$_TENANT" "$_PURPOSE" | \
         _format_output "$_FORMAT"
